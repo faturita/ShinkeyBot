ccc8ee155bb6d25bf4c9c20261544afd6c56c3c7
Rodrigo Ramele <rramele@gmail.com>
2016-05-11 03:32:24 +0000

    Fix issue with naming

--- NeoCortex/SensorimotorLogger.py
+++ NeoCortex/SensorimotorLogger.py
@@ -34,29 +34,30 @@ st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H-%M-%S')
 f = open('../data/sensor.'+st+'.dat', 'w')
 
 # Cancel sensor information.
-ser.write('X')
+ssmr.write('X')
 time.sleep(6)
 
 
-buf = ser.readline()
+buf = ssmr.readline()
 print str(buf)
 
-buf = ser.readline()
+buf = ssmr.readline()
 print str(buf)
 
-buf = ser.readline()
+buf = ssmr.readline()
 print str(buf)
 
 # Reactive sensor information
-ser.write('S')
+ssmr.write('S')
 
 
 while True:
   # read
-  myByte = ser.read(1)
+  ssmr.write('S')
+  myByte = ssmr.read(1)
   if myByte == 'S':
-      data = ser.read(32)
-      myByte = ser.read(1)
+      data = ssmr.read(32)
+      myByte = ssmr.read(1)
       if myByte == 'E':
           # is  a valid message struct
           new_values = unpack('fffhhhhhhff', data)
@@ -65,4 +66,5 @@ while True:
           f.write( str(new_values[6]) + ' ' + str(new_values[7]) + ' ' + str(new_values[8]) + '\n')
 
 f.close()
-ser.close()
+ssmr.close()
+mtrn.close()
