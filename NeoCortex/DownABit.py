9e81ba5997b8fcdff945ab3b43e0542c53dc6437
faturita <rramele@gmail.com>
2016-09-09 13:11:57 +0000

    Running on the new pi

--- NeoCortex/DownABit.py
+++ NeoCortex/DownABit.py
@@ -6,8 +6,8 @@ import os
 
 if (os.path.exists('/dev/tty.usbmodem1411')):
    ser = serial.Serial(port='/dev/tty.usbmodem1411',baudrate=115200,timeout=0)
-elif (os.path.exists('/dev/ttyACM1')):
-   ser = serial.Serial(port='/dev/ttyACM1',baudrate=115200,timeout=0)
+elif (os.path.exists('/dev/ttyACM0')):
+   ser = serial.Serial(port='/dev/ttyACM0',baudrate=115200,timeout=0)
 
 time.sleep(5)
 
