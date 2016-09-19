0e1ba6ee6228bdad5c7a3e76bc76e09e264b205f
Rodrigo Ramele <rramele@gmail.com>
2016-09-19 13:19:44 -0300

    New ARM code.  5 DOF

--- MotorUnitController/MotorUnitController.ino
+++ MotorUnitController/MotorUnitController.ino
@@ -3,32 +3,31 @@
  * 
  * It uses AFMotor.h Adafruit library and Shield (v1)
  * 
+ * Gripper 1 DC Motor -> M1
+ * Shoulder 2 DC Motor 12 V -> M2
+ * Tesaki 4 DC Motor del Servo roto 6 V -> M4
  * 
+ * Servo Wrist 9V 180 -> Servo 1 Pin 10
+ * Servo Elbow 9V 360 -> Servo 2 Pin 9 Coaxial
  */
 
 
 #include <AFMotor.h>
 #include <Servo.h> 
 
-bool debug = false;
+bool debug = true;
 
 // create motor objects, 64KHz pwm
-//AF_DCMotor laser(1, MOTOR12_64KHZ);
-//AF_DCMotor turret(4, MOTOR12_64KHZ);
-//AF_DCMotor grip(3, MOTOR12_64KHZ);
+AF_DCMotor grip(1, MOTOR12_64KHZ);
+AF_DCMotor tesaki(4, MOTOR12_64KHZ);
 AF_DCMotor shoulder(2, MOTOR12_64KHZ);
 
  
-Servo wrist;  // create servo object to control a servo 
-                // twelve servo objects can be created on most boards
-
-
-Servo grip;
-AF_DCMotor grip2(1, MOTOR12_64KHZ);
+Servo wrist; 
+Servo elbow;
 
 
 int state=0;
-
 int speeds = 255;
 
 
@@ -36,21 +35,14 @@ void setup() {
  Serial.begin(9600); // set up Serial library at 9600 bps
  Serial.println("Motor Unit Neuron");
 
- //shoulder.setSpeed(255);
- //grip.setSpeed(255);
- //turret.setSpeed(255);
- //laser.setSpeed(250);
-
- //grip.attach(10);  // Servo2 orange in
-
- wrist.attach(9);
+ wrist.attach(10);
+ elbow.attach(9);
 
 }
 
 int incomingByte = 0;
 
 char buffer[4];
-
 
 int direction=0;
 int pos=48;
@@ -83,6 +75,24 @@ void update(Servo servo) {
 
 } 
 
+void push(AF_DCMotor motor) {
+    motor.run(FORWARD);
+    motor.setSpeed(0);
+    delay(50);
+    motor.setSpeed(255);
+    delay(50);  
+}
+
+void pull(AF_DCMotor motor) {
+    motor.run(BACKWARD);
+    motor.setSpeed(0);
+    delay(50);
+    motor.setSpeed(255);
+    delay(250);  
+}
+
+int counter;
+
 void loop() {
 
  if (Serial.available() > 0) {
@@ -101,12 +111,21 @@ void loop() {
 
     if (syncbyte == 'A')
     {   
-      // Format A1000 >> A1220
-      // Format A5000
+      // Format A1000 >> A1220   --> Close grip
+      // A2255 >> Open Grip
+      // A6090 >> 90 deg wrist A6010 --> A6180
+      // A3220 or A4220 Move forward backward shoulder
+      // AA150 -> Keep elbow steady, AA140 backward AA160 forward (AA100 --> AA150 --> AA200)
+      // Format A5000  Reset everything.
       int readbytes = Serial.readBytes(buffer, 4);
       
       if (readbytes == 4) {
-        int action = buffer[0]-48;
+        Serial.println ( (int)buffer[0] );
+        int action = 0;
+        if (buffer[0] >= 65)  // send alpha hexa actions.
+          action = buffer[0]-65+10;
+        else
+          action = buffer[0]-48;
         int a = buffer[1]-48;
         int b = buffer[2]-48;
         int c = buffer[3]-48;
@@ -132,13 +151,13 @@ void loop() {
  {
   case 1: 
     //grip.write(speeds);
-    grip2.setSpeed(speeds);
-    grip2.run(FORWARD);
+    grip.setSpeed(speeds);
+    grip.run(FORWARD);
     break;
   case 2:
     //grip.write(-speeds);
-    grip2.setSpeed(speeds);
-    grip2.run(BACKWARD);
+    grip.setSpeed(speeds);
+    grip.run(BACKWARD);
     break;
   case 3:
     // Go up
@@ -150,8 +169,9 @@ void loop() {
     shoulder.run(BACKWARD);
     break;
   case 5:
-    shoulder.run(RELEASE);
-    grip2.run(RELEASE);
+    grip.run(RELEASE);
+    tesaki.run(RELEASE);
+    elbow.detach();
     state=0;
     break;
   case 6:
@@ -163,20 +183,28 @@ void loop() {
       direction =-1;
     break;
   case 8:
-    //grip.attach(10);
-    wrist.attach(9);
-    state=0;
+    tesaki.setSpeed(speeds);
+    tesaki.run(BACKWARD);
     break;
   case 9:
-    //grip.detach();   
-    wrist.detach();
-    state=0;
+    tesaki.setSpeed(speeds);
+    tesaki.run(FORWARD);
+    break;
+  case 0x0a:
+    // 150x10 is no movement. 360 servo.
+    elbow.writeMicroseconds(speeds*10);
+    counter=0;
     break;
    default:
     // Do Nothing
     state=0;
     break;
     
+ }
+ counter=counter+1;
+ if (counter>=1000) {
+  elbow.writeMicroseconds(1500);
+  counter=0;
  }
 
  delay(10);
--- NeoCortex/NeoCortex.py
+++ NeoCortex/NeoCortex.py
@@ -1,5 +1,14 @@
 #coding: latin-1
 
+# NeoCortex is the core program to control ShinkeyBot
+# It handles basic USB-Serial comm with other modules and handles
+# the basic operation of ShinkeyBot
+#
+# x) Transmit TCP/IP images through CameraStreamer.
+# x) Captures sensor data from SensorimotorLogger
+# x) Handles output to motor unit and sensorimotor commands through Proprioceptive
+# x) Receives high-level commands from ShinkeyBotController.
+
 import numpy as np
 import cv2
 
@@ -13,28 +22,30 @@ import sys, os, select
 import socket
 
 import Proprioceptive as prop
+import thread
 import PicameraStreamer as pcs
 import SensorimotorLogger as senso
 
 import VisualCortex as visual
 
+# Get PiCamera stream and read everything in another thread.
 obj = pcs.VideoStreamer()
-
-import thread
-
 thread.start_new_thread( obj.connect, () )
 
 
 # Initialize UDP Controller Server on port 10001
+# This receives high-level commands from ShinkeyBotController.
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('0.0.0.0', 10001)
 print >> sys.stderr, 'Starting up Controller Server on %s port %s', server_address
 sock.bind(server_address)
 
+# Open connection to tilt sensor.
 hidraw = prop.setupsensor()
-
+# Open serial connection to MotorUnit and Sensorimotor Arduinos.
 [ssmr, mtrn] = prop.serialcomm()
 
+# Handle connection to the visual turret (defunct)
 turret = visual.Turret()
 turret.init()
 
@@ -42,84 +53,105 @@ turret.init()
 ssmr.write('C')
 
 tgt = -1000
-
 wristpos=48
-
 sensesensor = 0
 
+# Connect remotely to any client that is waiting for sensor loggers.
 sensorimotor = senso.Sensorimotor()
 sensorimotor.start()
 sensorimotor.cleanbuffer(ssmr,mtrn)
 
+# Live
 while(True):
     try:
         data, address = sock.recvfrom(1)
 
+        # If someone asked for it, send sensor information.
         if (sensesensor == 1):
             sensorimotor.sendsensorsample(ssmr,mtrn)
 
         if (data == 'H'):
-            ssmr.write('H')  
+            ssmr.write('H')
             #Right
         elif (data == 'G'):
-            ssmr.write('G')  
+            ssmr.write('G')
             #Center
         elif (data == 'F'):
-            ssmr.write('F')   
+            ssmr.write('F')
             #Left
         elif (data == 'T'):
-            ssmr.write('T')  
+            ssmr.write('T')
             #Down nose
         elif (data == 'Y'):
             mtrn.write('A3250')
             time.sleep(0.8)
             mtrn.write('A5000')
+            # Arm down
         elif (data=='J'):
             # mtrn.write('A6180')
             wristpos = wristpos + 1
             mtrn.write('A6'+'{:3d}'.format(wristpos))
+            # wrist Up
         elif (data=='j'):
             # mtrn.write('A6090')
             wristpos = wristpos - 1
             mtrn.write('A6'+'{:3d}'.format(wristpos))
+            # wrist down
         elif (data == 'H'):
             mtrn.write('A4250')
             time.sleep(0.2)
             mtrn.write('A5000')
+            # Arm Up
         elif (data=='G'):
             mtrn.write('A1000')
             time.sleep(2)
+            # Gripper
         elif (data=='R'):
             mtrn.write('A1200')
             time.sleep(2)
+            # Gripper Release
         elif (data==' '):
             ssmr.write('1')
+            # Quiet
         elif (data=='W'):
             ssmr.write('2')
+            # Forward
         elif (data=='S'):
             ssmr.write('3')
+            # Backward
         elif (data=='D'):
             ssmr.write('5')
+            # Right
         elif (data=='A'):
             ssmr.write('4')
+            # Left
         elif (data=='.'):
             ssmr.write('-')
+            # Move slowly
         elif (data==','):
             ssmr.write('+')
+            # Move coarsely
         elif (data=='L'):
             ssmr.write('L')
+            # Laser on
         elif (data=='l'):
             ssmr.write('l')
+            # Laser off
         elif (data=='+'):
             tgt = tgt + 100
+            # Pull up tesaki target
         elif (data=='-'):
             tgt = tgt - 100
+            # Pull down tesaki target
         elif (data=='M'):
             prop.moveto(mtrn, hidraw, tgt)
+            # PID to desired position
         elif (data=='E'):
             ssmr.write('E')
+            # Empire song
         elif (data=='B'):
             ssmr.write('B')
+            # Buzz
         elif (data=='X'):
             break
     except:
