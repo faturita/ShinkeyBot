8152ab0155095bf8b01e36f8eecc6dcf5a8f9ac2
faturita <rramele@gmail.com>
2017-05-17 01:13:45 -0300

    Update client to apply a filter on the view camera to detect laser pointers thus meassuring distances

--- NeoCortex/IPCamera.py
+++ NeoCortex/IPCamera.py
@@ -1,6 +1,8 @@
 #coding: latin-1
 import cv2
 import time
+import numpy as np
+import argparse
 
 import Configuration as conf
 
@@ -14,29 +16,71 @@ cap = cv2.VideoCapture('tcp://192.168.0.110:10000')
 
 print ("Connecting..")
 
+def click_and_crop(event, x, y, flags, param):
+
+	# if the left mouse button was clicked, record the starting
+	# (x, y) coordinates and indicate that cropping is being
+	# performed
+	if event == cv2.EVENT_RBUTTONDOWN:
+		print (x, y)
+
+cv2.namedWindow("ShinkeyBot Eye")
+cv2.setMouseCallback("ShinkeyBot Eye", click_and_crop)
+
+
+def hough(frame):
+    edges = cv2.Canny(frame,50,150,apertureSize = 3)
+    minLineLength = 100
+    maxLineGap = 10
+    lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+    for x1,y1,x2,y2 in lines[0]:
+        cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),2)
+	return frame
+
+
+
 for i in range(1,80000):
-   # Capture frame-by-frame
-   ret, frame = cap.read()
+	# Capture frame-by-frame
+	ret, frame = cap.read()
 
-   #frame = cv2.flip(frame,0)
-   #frame = cv2.flip(frame,1)
+	#frame = cv2.flip(frame,0)
+	#frame = cv2.flip(frame,1)
 
-   #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-   #cv2.imwrite('01.png', gray)
+	#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	#cv2.imwrite('01.png', gray)
 
-   #Using AKAZE descriptors.
-   #detector = cv2.AKAZE_create()
-   #(kps, descs) = detector.detectAndCompute(gray, None)
-   #print("keypoints: {}, descriptors: {}".format(len(kps), descs.shape))
+	#Using AKAZE descriptors.
+	#detector = cv2.AKAZE_create()
+	#(kps, descs) = detector.detectAndCompute(gray, None)
+	#print("keypoints: {}, descriptors: {}".format(len(kps), descs.shape))
 
-   # draw the keypoints and show the output image
-   #cv2.drawKeypoints(frame, kps, frame, (0, 255, 0))
+	# draw the keypoints and show the output image
+	#cv2.drawKeypoints(frame, kps, frame, (0, 255, 0))
 
-   cv2.imshow("ShinkeyBot Eye", frame)
+	#edges = cv2.Canny(frame,100,200)
+	#edges = cv2.Canny(frame,50,150,apertureSize = 3)
+
+	# Convert BGR to HSV
+	# Convert BGR to HSV
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	# define range of blue color in HSV
+	lower_blue = np.array([160,50,50])
+	upper_blue = np.array([185,255,255])
 
 
-   if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
+	# Threshold the HSV image to get only blue colors
+	mask = cv2.inRange(hsv, lower_blue, upper_blue)
+	# Bitwise-AND mask and original image
+	res = cv2.bitwise_and(frame,frame, mask= mask)
+	res = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+	hough(res)
+
+	#print frame[474,37]
+
+	cv2.imshow("ShinkeyBot Eye", res)
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+	    break
 
 print ('Done.')
 
